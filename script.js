ecc7e198b3e248c469162af727dcbeedafe1c5dc
faizurrehman39804-eng <faizurrehman39804@gmail.com>
2026-05-21 14:00:59 +0500

    add the arthaetic opreter exmpple and practice Question

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // External JavaScript definition:
 // alert('This is an external JavaScript alert!');
 
+// ⭕⭕⭕✨✨✨variables in JavaScript
 // 1️⃣var
 // var a = "hooo";
 // console.log(a);
@@ -12,20 +13,39 @@
 // let c = "hooo";
 // console.log(c);
 
-//3️⃣const 
+//3️⃣const `
 // const d = "hooo";
 //  d = "hooo";
 // const d = "hooo";
 // console.log(d);
 
-//4️⃣Data Types
-var x = "faiz ur rehman";
+//⭕⭕⭕✨✨✨4️⃣Data Types
+// var x = "faiz ur rehman";
 // var x = 30;  
 // var x = true;  
 // var x = null;
 // var x = undefined;
-var x = ["faiz ur rehman", "Peter", "true", "null", "undefined"];
+// var x = ["faiz ur rehman", "Peter", "true", "null", "undefined"];
 
+// document.write(x);
 
-document.write(x);
-document.write( "<br>" + typeof x);
+//⭕⭕⭕✨✨✨5️⃣Operators
+// +
+// var a = 5;
+// var b = 5;
+// var sum = a + b;
+
+// document.write(sum);
+
+// -
+// var a = 15;
+// var b = 5;
+// var sum = a - b;
+// 
+// document.write(sum);
+
+// *division {5x5=25}
+var a = 5;
+var b = 5;
+var sum = a * b;
+document.write(sum);
